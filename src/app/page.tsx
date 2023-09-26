f8a1318832cbb013b69329c1df3e0e9729ab59f5
Nick Little <n14little@gmail.com>
2023-09-25 21:50:03 -0500

    feat: add interactive quiz to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-start p-24">
       <p>Help yourself stand out by highlighting key words in your resume.</p>
+      <iframe id="interactApp65123c3d77e3c90014ce11a1" width="800" height="800" style={{ border: 'none', 'maxWidth': '100%', margin: 0 }} src="https://quiz.tryinteract.com/#/65123c3d77e3c90014ce11a1?method=iframe"></iframe>
     </main>
   )
 }
